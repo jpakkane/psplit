68ba8f3611dd2a3610dd359a8c45179bc77ceaff
Jussi Pakkanen <jpakkane@gmail.com>
2021-03-22 18:33:32 +0200

    Import proto code to Git.

---
+++ psplit.hpp
@@ -0,0 +1,176 @@
+#pragma once
+
+/*
+Copyright (c) 2021 Jussi Pakkanen
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include <string_view>
+#include <vector>
+#include <string>
+#include <filesystem>
+
+namespace psplit {
+
+enum class Empties : char { Preserve, Drop };
+
+#ifdef _WIN32
+class MmapFile final {
+public:
+    explicit MmapFile(const std::filesystem::path &fname) {
+        filehandle = CreateFile(fname.wstring().c_str(),
+                                GENERIC_READ,
+                                0,
+                                nullptr,
+                                OPEN_EXISTING,
+                                FILE_ATTRIBUTE_NORMAL,
+                                nullptr);
+        mappinghandle = CreateFileMapping(filehandle, nullptr, PAGE_READONLY, 0, 0, nullptr);
+    }
+
+    std::string_view view() const noexcept {
+        LARGE_INTEGER fs;
+        GetFileSizeEx(filehandle, &fs);
+        auto *addr = static_cast<char *>(MapViewOfFile(mappinghandle, FILE_MAP_READ, 0, 0, 0));
+        if(!addr) {
+            return std::string_view{};
+        }
+        return std::string_view(addr, fs.QuadPart);
+    }
+
+    ~MmapFile() { unmap(); }
+
+    MmapFile() = delete;
+    MmapFile(const MmapFile &) = delete;
+    MmapFile(MmapFile &&o) noexcept : filehandle(o.filehandle), mappinghandle(o.mappinghandle) {
+        o.filehandle = o.mappinghandle = nullptr;
+    }
+    MmapFile &operator=(const MmapFile &) = delete;
+    MmapFile &operator=(MmapFile &&o) noexcept {
+        if(this != &o) {
+            unmap();
+            filehandle = o.filehandle;
+            mappinghandle = o.mappinghandle;
+            o.filehandle = o.mappinghandle = nullptr;
+        }
+    }
+
+private:
+    void unmap() {
+        if(mappinghandle) {
+            CloseHandle(mappinghandle);
+            CloseHandle(filehandle);
+            filehandle = mappinghandle = nullptr;
+        }
+    }
+    HANDLE filehandle;
+    HANDLE mappinghandle;
+};
+
+#endif
+
+inline void
+add_piece(std::vector<std::string> &words, std::string_view substr, const Empties e) noexcept {
+    if(e == Empties::Drop && substr.empty()) {
+        return;
+    }
+    words.push_back(std::string(substr));
+}
+
+inline std::vector<std::string> split_copy(std::string_view input,
+                                           const char split_chr = '\n',
+                                           const Empties e = Empties::Drop) noexcept {
+    std::vector<std::string> words;
+    std::string_view::size_type current = 0;
+    if(input.empty()) {
+        add_piece(words, input.substr(0, 0), e);
+        return words;
+    }
+    const auto input_size = input.size();
+    while(current != std::string_view::npos && current != input_size) {
+        if(input[current] == split_chr) {
+            if(current == 0) {
+                add_piece(words, input.substr(current, 0), e);
+            } else {
+                if(input[current - 1] == split_chr) {
+                    add_piece(words, input.substr(current, 0), e);
+                }
+            }
+            ++current;
+        } else {
+            auto end = input.find(split_chr, current);
+            if(end == std::string_view::npos) {
+                add_piece(words, input.substr(current, end), e);
+            } else {
+                add_piece(words, input.substr(current, end - current), e);
+            }
+            current = end;
+        }
+    }
+    if(input.back() == split_chr) {
+        add_piece(words, input.substr(input_size, 0), e);
+    }
+    return words;
+}
+
+inline std::vector<std::string_view> split_lines(std::string_view data) noexcept {
+    std::vector<std::string_view> lines;
+    std::string_view newlines("\r\n");
+    size_t current = 0;
+    if(data.empty()) {
+        return lines;
+    }
+    while(current < data.size()) {
+        auto end = data.find_first_of(newlines, current);
+        if(current == end) {
+            lines.emplace_back("");
+        } else if(end == std::string_view::npos) {
+            lines.emplace_back(data.substr(current, end));
+            end = data.size() - 1;
+        } else {
+            lines.emplace_back(data.substr(current, end - current));
+        }
+
+        if(data[end] == '\r' && end + 1 < data.size() && data[end + 1] == '\n') {
+            current = end + 2;
+        } else {
+            current = end + 1;
+        }
+    }
+    return lines;
+}
+
+inline std::vector<std::string> split_lines_copy(std::string_view data) noexcept {
+    std::vector<std::string> lines;
+    auto line_views = split_lines(data);
+    lines.reserve(line_views.size());
+    for(const auto &s : line_views) {
+        lines.emplace_back(s);
+    }
+    return lines;
+}
+
+inline std::vector<std::string> split_file_copy(const std::filesystem::path &path) noexcept {
+    MmapFile mf(path);
+    return split_lines_copy(mf.view());
+}
+} // namespace psplit
---
+++ psplit_test.cpp
@@ -0,0 +1,229 @@
+/*
+Copyright (c) 2021 Jussi Pakkanen
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "psplit.hpp"
+#include <iostream>
+
+int validate(const std::vector<std::string> &a1, const std::vector<std::string> &a2) {
+    if(a1.size() != a2.size()) {
+        std::cout << "Array size mismatch: " << a1.size() << " vs " << a2.size() << ".\n";
+        return 1;
+    }
+    for(size_t i = 0; i < a1.size(); ++i) {
+        if(a1[i] != a2[i]) {
+            std::cout << "Index #" << i << " mismatch: " << a1[i] << " vs " << a2[i] << ".\n";
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int check_splits(const std::string &input,
+                 const std::vector<std::string> &truth_preserved,
+                 const std::vector<std::string> &truth_drop) {
+    auto preserved = psplit::split_copy(input, '\n', psplit::Empties::Preserve);
+    auto dropped = psplit::split_copy(input, '\n', psplit::Empties::Drop);
+
+    std::cout << "  preserving empties\n";
+    if(validate(preserved, truth_preserved) != 0) {
+        return 1;
+    }
+    std::cout << "  dropping empties.\n";
+    return validate(dropped, truth_drop);
+}
+
+int test1() {
+    std::string input{"a\nb\nc"};
+    const std::vector<std::string> truth_preserve{{"a"}, {"b"}, {"c"}};
+    const std::vector<std::string> truth_drop{{"a"}, {"b"}, {"c"}};
+
+    return check_splits(input, truth_preserve, truth_drop);
+}
+
+int test2() {
+    std::string input{"a\n\nb"};
+    const std::vector<std::string> truth_preserve{{"a"}, {""}, {"b"}};
+    const std::vector<std::string> truth_drop{{"a"}, {"b"}};
+
+    return check_splits(input, truth_preserve, truth_drop);
+}
+
+int test3() {
+    std::string input{"\na\n"};
+    const std::vector<std::string> truth_preserve{{""}, {"a"}, {""}};
+    const std::vector<std::string> truth_drop{{"a"}};
+
+    return check_splits(input, truth_preserve, truth_drop);
+}
+
+int test4() {
+    std::string input{"\n\n\n"};
+    const std::vector<std::string> truth_preserve{{""}, {""}, {""}, {""}};
+    const std::vector<std::string> truth_drop{};
+
+    return check_splits(input, truth_preserve, truth_drop);
+}
+
+int test5() {
+    std::string input{"\n"};
+    const std::vector<std::string> truth_preserve{{""}, {""}};
+    const std::vector<std::string> truth_drop{};
+
+    return check_splits(input, truth_preserve, truth_drop);
+}
+
+int test6() {
+    std::string input{"\n\n\nx\n\n\n"};
+    const std::vector<std::string> truth_preserve{{""}, {""}, {""}, "x", {""}, {""}, {""}};
+    const std::vector<std::string> truth_drop{{"x"}};
+
+    return check_splits(input, truth_preserve, truth_drop);
+}
+
+int test7() {
+    std::string input{""};
+    const std::vector<std::string> truth_preserve{{""}};
+    const std::vector<std::string> truth_drop{};
+
+    return check_splits(input, truth_preserve, truth_drop);
+}
+
+int test8() {
+    std::string input{"abcd"};
+    const std::vector<std::string> truth_preserve{{"abcd"}};
+    const std::vector<std::string> truth_drop{"abcd"};
+
+    return check_splits(input, truth_preserve, truth_drop);
+}
+
+int test_lines() {
+    std::string input1("\nhello\nworld\n");
+    std::string input2("\r\nhello\r\nworld\r\n");
+    const std::vector<std::string> truth{{""}, {"hello"}, {"world"}};
+
+    auto result = psplit::split_lines_copy(input1);
+    if(validate(result, truth) != 0) {
+        return 1;
+    }
+    result = psplit::split_lines_copy(input2);
+    return validate(result, truth);
+}
+
+int test_lines2() {
+    std::string input1("hello\nworld");
+    std::string input2("hello\r\nworld");
+    const std::vector<std::string> truth{{"hello"}, {"world"}};
+
+    auto result = psplit::split_lines_copy(input1);
+    if(validate(result, truth) != 0) {
+        return 1;
+    }
+    result = psplit::split_lines_copy(input2);
+    return validate(result, truth);
+}
+
+int test_lines3() {
+    std::string input1("hello\n\nworld");
+    std::string input2("hello\r\n\r\nworld");
+    const std::vector<std::string> truth{{"hello"}, {""}, {"world"}};
+
+    auto result = psplit::split_lines_copy(input1);
+    if(validate(result, truth) != 0) {
+        return 1;
+    }
+    result = psplit::split_lines_copy(input2);
+    return validate(result, truth);
+}
+
+int test_file() {
+    std::filesystem::path path("input_unix.txt");
+    const std::vector<std::string> truth_preserve{{"abc"}, {"def"}};
+
+    auto result = psplit::split_file_copy(path);
+    return validate(result, truth_preserve);
+}
+
+int test_file2() {
+    std::filesystem::path path("input_dos.txt");
+    const std::vector<std::string> truth_preserve{{"abc"}, {"def"}};
+
+    auto result = psplit::split_file_copy(path);
+    return validate(result, truth_preserve);
+}
+
+int main() {
+    std::cout << "Test 1\n";
+    if(test1() != 0) {
+        return 1;
+    }
+    std::cout << "Test 2\n";
+    if(test2() != 0) {
+        return 1;
+    }
+    std::cout << "Test 3\n";
+    if(test3() != 0) {
+        return 1;
+    }
+    std::cout << "Test 4\n";
+    if(test4() != 0) {
+        return 1;
+    }
+    std::cout << "Test 5\n";
+    if(test5() != 0) {
+        return 1;
+    }
+    std::cout << "Test 6\n";
+    if(test6() != 0) {
+        return 1;
+    }
+    std::cout << "Test 7\n";
+    if(test7() != 0) {
+        return 1;
+    }
+    std::cout << "Test 8\n";
+    if(test8() != 0) {
+        return 1;
+    }
+
+    std::cout << "Test lines\n";
+    if(test_lines() != 0) {
+        return 1;
+    }
+
+    std::cout << "Test lines 2\n";
+    if(test_lines2() != 0) {
+        return 1;
+    }
+    std::cout << "Test lines 3\n";
+    if(test_lines3() != 0) {
+        return 1;
+    }
+
+    std::cout << "Test file\n";
+    if(test_file() != 0) {
+        return 1;
+    }
+
+    std::cout << "Test file 2\n";
+    if(test_file2() != 0) {
+        return 1;
+    }
+    return 0;
+}
